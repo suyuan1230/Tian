bf9ffdc6a29b4f10b4e74846be8c9747c3a7777a
suyuan1230 <111753655+suyuan1230@users.noreply.github.com>
2022-08-22 22:49:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app_id = 'wx4e10a40f179ef02b'
 app_secret = '8b55fa7e2a50131e4944dc95d6d1eb88'
 
 user_id = 'oPcoa6yrqg4orcqb_eimFeWAaKsE'
-template_id = 'Etg4KYxt3o9xhrsf6aL3pyiEuQWD-RRkZ4gBxVi74pY'
+template_id = 'uctMuKwSn3G5vgJ0w4RaPieXGrRnmno-b7B9Yr3wzeE'
 
 
 def get_weather():
